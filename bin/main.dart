25aa7acf9bcf1d9c0cd6e3ad6973700320d35e21
AshishTaduce <ashishteddy2@gmail.com>
2019-10-15 09:21:28 +0530

    initial commit

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 // Challenge 1
 // Create a function that returns the total number of steps it takes to transform each element to the maximal element in the list.
 // Each step consists of incrementing a digit by one.
@@ -7,6 +8,19 @@
 // incrementToTop([4, 3, 4]) ➞ 1
 // incrementToTop([3, 3, 3]) ➞ 0
 // incrementToTop([3, 10, 3]) ➞ 14
+
+int incrementToTop(List list){
+  int count = 0;
+  list.sort();
+  int max = list.last;
+  for (int x in list){
+    while (x < max){
+      x++;
+      count++;
+    }
+  }
+  return count;
+}
 
 // Challenge 2
 // In BlackJack, cards are counted with -1, 0, 1 values:
@@ -21,7 +35,27 @@
 //
 //  count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]) ➞ 5
 
+int count (List list){
+  int count = 0;
+  List all = [[2,3,4,5,6],[7,8,9],[10,'J','Q','K','A',],];
+  for (var x in list){
+    if(all[0].contains (x)){
+      count++;
+    }
+    else if(all[2].contains (x)){
+      count--;
+    }
+  }
+  return count;
+}
+
 // Challenge 3
 // Write a test case each for previous two functions you wrote
 
-main() {}
+main() {
+  print(count(["A", 5, 5, 2, 6, 2, 3, 8, 9, 7]));
+  print(count(["A", "A", "K", "Q", "Q", "J"]));
+  print(incrementToTop([3, 10, 3]));
+  print(incrementToTop([4, 3, 4]));
+  print(count([5, 9, 10, 3, "J", "A", 4, 8, 5]));
+}
--- bin/test.dart
+++ bin/test.dart
@@ -1,3 +1,14 @@
 import 'package:test/test.dart';
+import 'main.dart';
 
-void main() {}
+void main() {
+  test("Create a function that returns the total number of steps it takes to transform each element to the maximal element in the list.", () {
+    var List = [3, 10, 3];
+    expect(incrementToTop(List), equals (14));
+  });
+
+  test("Create a function that counts the number and returns it from the list of cards provided.", () {
+    var list = [5, 9, 10, 3, "J", "A", 4, 8, 5];
+    expect(count(list), equals (1));
+  });
+}
